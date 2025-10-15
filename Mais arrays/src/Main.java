ed241d834b4ac7f187f6c47efe388e1d85a7cc4c
unknown <gui.franklin17@gmail.com>
2025-10-15 10:23:25 -0300

    funcionarios de loja

--- Mais arrays/src/Main.java
+++ Mais arrays/src/Main.java
@@ -1,14 +1,17 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 
 public class Main {
     public static void main(String[] args) {
         ArrayList<String> produtos = new ArrayList();
 
-        produtos.add("maçã");
-        produtos.add("aveia");
-        produtos.add("leite");
-        produtos.add("frango");
+        produtos.add("4- maçã");
+        produtos.add("1- aveia");
+        produtos.add("3- leite");
+        produtos.add("2- frango");
+
+        Collections.sort(produtos);
 
         //imprime array
         for(String compras: produtos){
@@ -16,13 +19,13 @@ public class Main {
         }
 
         //descobre o indice do elemento
-        System.out.println(produtos.indexOf("leite"));
+        System.out.println(produtos.indexOf("3- leite"));
 
-        //diz se a lista está fazia
+        //diz se a lista está vazia
         System.out.println(produtos.isEmpty());
 
         //verifica se a lista possui tal elemento
-        System.out.println(produtos.contains("frango"));
+        System.out.println(produtos.contains("2- frango"));
 
         //quantos elementos tem na lista
         System.out.println(produtos.size());
